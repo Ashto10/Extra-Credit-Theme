5156ec577d1ec426bbba8f0ff9a4cf3c6f10a477
Ashto <ashto13@hotmail.com>
2018-08-13 19:50:41 -0400

    Added title checking functionality
    - Upload files will be checked to assure titles are consistant
    - Titles will be corrected in most cases
    - Page will now give warnings for non-fatal errors
    - Variable names corrected to camelcase

--- upload.php
+++ upload.php
@@ -4,11 +4,12 @@
  * Description: 
  */
 
-$target_dir = getcwd() . "/episodes/";
-$file_name = basename($_FILES["fileToUpload"]["name"]);
-$target_file = $target_dir . $file_name;
+$targetDir = getcwd() . "/episodes/";
+$fileName = basename($_FILES["fileToUpload"]["name"]);
+$targetFile = $targetDir . $fileName;
+$uploadWarnings = [];
 $uploadErrors = [];
-$uploadFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$uploadFileType = strtolower(pathinfo($targetFile,PATHINFO_EXTENSION));
 
 if(isset($_POST["submit"])) {
 
@@ -16,17 +17,39 @@ if(isset($_POST["submit"])) {
     $uploadErrors[] = "Only MP3 files are allowed";
   }
 
+  if(!preg_match("/\.\d{3}\.mp3$/", $fileName)) {
+    $match = [];
+    preg_match("/\.?(\d+).mp3$/", $fileName, $match);
+    $correction = '.' . str_pad($match[1], 3, '0', STR_PAD_LEFT) . '.mp3';
+    $correction = str_replace($match[0], $correction, $fileName);
+
+    if(preg_match("/\.\d{3}\.mp3$/", $correction)) {
+      $uploadWarnings[] = "Your file $fileName was not titled properly. It has been renamed to $correction";
+      $fileName = $correction;
+      $targetFile = $targetDir . $fileName;
+    } else {
+      $uploadErrors[] = "Your file title does not match the following format: name.###.mp3";
+    }
+  }
+
   if ($_FILES["fileToUpload"]["size"] > 200000000) {
     $uploadErrors[] = "Your file is too large! Please limit the filesize to under 200MB";
   }
 
-  if (file_exists($target_file) && !isset($_POST['overwrite'])) {
+  if (file_exists($targetFile) && !isset($_POST['overwrite'])) {
     $uploadErrors[] = "This file already exists. If this is your intent, make sure to check off the \"overwrite\" option.";
   }
 
   if (count($uploadErrors) === 0) {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-      echo "<div class='alert alert-success'><p>The file ". $file_name . " has been uploaded.</p></div>";
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
+      if (count($uploadWarnings) > 0) {
+        echo "<div class='alert alert-warning'><ul>";
+        foreach($uploadWarnings as $warning) {
+          echo "<li>$warning</li>";
+        }
+        echo "</ul></div>";
+      }
+      echo "<div class='alert alert-success'><p>The file ". $fileName . " has been uploaded.</p></div>";
     } else {
       echo "<div class='alert alert-danger'><p>Sorry, something has gone wrong. Please try again later, or contact an admin if the issue persists.</p></div>";
     }
